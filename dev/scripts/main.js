1c3c58b53c4b334c310a758356a8168b69fddf63
Estela <estelathomson@me.com>
2017-07-30 19:34:55 -0400

    init commit

---
+++ dev/scripts/main.js
@@ -0,0 +1,55 @@
+
+$(function(){
+	// initiate Isotope
+	// var createGrid = function() {
+	var $grid = $('.gallery').isotope({
+		itemSelector: '.gallery_item',
+		layoutMode: 'fitRows'
+	});
+// };
+
+	// Isotope events
+	$('.nav_portfolio').on('click', 'li', function(){
+		var selector = $(this).attr('data-filter');
+		// $('.nav_portfolio li').removeClass('active');
+		$grid.isotope({
+			// console.log(selector);
+			filter: selector
+		});
+	});
+
+	$('.nav_portfolio').each(function(i,buttons){
+		var $buttons = $(buttons);
+		$buttons.on('click', 'li', function(){
+			$buttons.find('.active').removeClass('active');
+			$(this).addClass('active');
+		});
+	});
+
+	//			 $(this).toggleClass('active');
+
+	// });
+	// return false;
+
+	// reveal skill items on scroll
+	window.sr = ScrollReveal();
+	sr.reveal('.skills_item', { duration: 2000 }, 500);
+
+
+	// smooth scroll on anchor tags
+	$('a[href*="#"]:not([href="#"])').click(function() {
+	  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	    var target = $(this.hash);
+	    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	    if (target.length) {
+	      $('html, body').animate({
+	        scrollTop: target.offset().top
+	      }, 1000);
+	      return false;
+	    }
+	  }
+	});
+
+
+	
+});  // end of doc ready
